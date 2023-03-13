e577125f4bf39dbe674cc4c2269fb55f448c668c
MaN1Max <118527003+MaN1Max@users.noreply.github.com>
2023-03-13 21:20:41 +0300

    FirstBKP
    BKP

---
+++ WebAppPractice2023/WebAppPractice2023/Data/Interfaces/ITenants.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using WebAppPractice2023.Data.Models;
+
+namespace WebAppPractice2023.Data.Interfaces
+{
+    public interface ITenants
+    {
+        IEnumerable<Tenants> Tenant { get; set; }
+        IEnumerable<Tenants> GetAllTenants { get; }
+        Tenants GetTenants(int tenantId);
+    }
+}
---
+++ WebAppPractice2023/WebAppPractice2023/ViewModels/TenantsIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using WebAppPractice2023.Data.Models;
+
+namespace WebAppPractice2023.ViewModels
+{
+    public class TenantsIndexViewModel
+    {
+        public IEnumerable<Tenants> AllTenants { get; set; }
+    }
+}
