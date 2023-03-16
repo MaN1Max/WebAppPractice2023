e0c9653ddfa8a02bd9af9abde305a5594474072f
MaN1Max <118527003+MaN1Max@users.noreply.github.com>
2023-03-16 20:55:39 +0300

    WebApp
    ReUpload

---
+++ WebAppMVCPractice/WebAppMVCPractice/Domain/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppMVCPractice.Models;
+
+namespace WebAppMVCPractice.Domain
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Tenant> Tenant { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Tenant>().HasData(new Tenant
+            {
+                Id = 1,
+                Surn = "Иванов",
+                Name = "Иван",
+                Patr = "Иванович",
+                Email = "te.st@gmail.com",
+                Phone = "+12345678901"
+            });
+        }
+    }
+}
